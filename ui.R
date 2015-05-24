121b366b636c5d534692153261569f56960bd877
Yuliang Wang <yuliangwang86@gmail.com>
2015-05-23 20:05:17 -0700

    correct misspelling

--- ui.R
+++ ui.R
@@ -4,8 +4,8 @@ shinyUI(pageWithSidebar(
     sidebarPanel(
         p('The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m2.'),
         p("In the original formula, the exponent for height is 2, though our world is 3D. You might think that the exponent should simply be 3, but that doesn't match the data at all - people don't scale in a perfectly linear fashion as they grow."),
-        p("Prof.Trehfethen from University of Oxford proposes that a better approximation to the actual sizes and shapes of healthy bodies might be given by an exponent of 2.5."),
-        p("When you provide your weight (in kilograms) and your height (in centimeters), this shiny app will calculate BMI based on both the original formula and the new formula proposed by Prof.Trehfethen. Then it will tell you whether you are underweight, normal, overweight or obese."),
+        p("Prof.Trefethen from University of Oxford proposes that a better approximation to the actual sizes and shapes of healthy bodies might be given by an exponent of 2.5."),
+        p("When you provide your weight (in kilograms) and your height (in centimeters), this shiny app will calculate BMI based on both the original formula and the new formula proposed by Prof.Trefethen. Then it will tell you whether you are underweight, normal, overweight or obese."),
         p("In addtion, it will also calculate the minimum amount of weight loss/gain necessary to achieve normal BMI range (18.5-25) based on both formula."),
         textInput(inputId="weight",label="Your weight in kg"),
         textInput(inputId="height",label="Your height in cm"),
